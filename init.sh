5fa0a35dea33db73f3911f196006e3d21432efd0
denmigda <denis.migdal@uca.fr>
2026-04-21 09:54:47 +0200

    template -> templates

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 WF_ROOT=$(dirname "$0")
 echo "* Found WebpackFramework root: $WF_ROOT"
 
-cp --update=none -r "$WF_ROOT/template/default/"{*,.[^.]*} ./
+cp --update=none -r "$WF_ROOT/templates/default/"{*,.[^.]*} ./
 
 mkdir dist
 
